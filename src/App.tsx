450ff1818814540b41d1f4cc266bc862c0939b1c
John Paolo Lumanlan <johnpaololumanlan@Johns-MacBook-Air.local>
2026-06-24 06:02:54 +0800

    full width update

--- src/App.tsx
+++ src/App.tsx
@@ -150,7 +150,7 @@ export default function App() {
 
   return (
     <div className="min-h-screen bg-slate-50 p-4 md:p-8 font-sans text-black">
-      <div className="max-w-5xl mx-auto">
+      <div className="w-full mx-auto">
         
         {/* Header */}
         <div className="text-center mb-10">
